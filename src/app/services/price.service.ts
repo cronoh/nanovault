34c5064b1d7cc6d8ece545ce3bd8285de86c3ed9
millsii3 <millsii3@Millsii3.local>
2019-07-19 19:41:48 +0100

    Currency  API
    Add currency API from coingecko
    
    Thanks to David_99

--- src/app/services/price.service.ts
+++ src/app/services/price.service.ts
@@ -4,7 +4,7 @@ import {BehaviorSubject} from "rxjs/BehaviorSubject";
 
 @Injectable()
 export class PriceService {
-  apiUrl = `https://api.coinmarketcap.com/v1/`;
+  apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=banano&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false&vs_currencies=btc,usd`;
 
   price = {
     lastPrice: 0.00,
@@ -15,17 +15,23 @@ export class PriceService {
   constructor(private http: HttpClient) { }
 
   async getPrice(currency = 'USD') {
-    if (!currency) return; // No currency defined, do not refetch
-    const convertString = currency !== 'USD' && currency !== 'BTC' ? `?convert=${currency}` : ``;
-    //const response: any = await this.http.get(`${this.apiUrl}ticker/banano/${convertString}`).toPromise();
-    if (!Response || !Response.length) {
-      return this.price.lastPrice;
-    }
 
-    const quote = Response[0];
-    const currencyPrice = quote[`price_${currency.toLowerCase()}`];
-    const btcPrice = quote.price_btc;
-    const usdPrice = quote.price_usd;
+    if (!currency) return; // No currency defined, do not refetch
+
+    const convertString = currency !== 'USD' && currency !== 'BTC' ? `,${currency}` : ``;
+
+    const response: any = await this.http.get(`${this.apiUrl}${convertString}`).toPromise();
+
+    // if (!Response || !Response.length) {
+    //   return this.price.lastPrice;
+    // }
+
+    const quote = response;
+
+    const currencyPrice = quote.banano[currency.toLowerCase()];
+    const usdPrice = quote.banano.usd;
+    const btcPrice = quote.banano.btc;
+
 
     this.price.lastPrice = currencyPrice;
     this.price.lastPriceBTC = btcPrice;
